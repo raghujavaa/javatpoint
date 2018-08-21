f93a85ef4c0445013bff7b31c74d6b415fe60815
Raghunadh <Raghunadh@192.168.2.9>
2018-08-21 13:50:01 +0530

    1:50 PM jdbc committ

---
+++ src/ExceptionAndStringInterviewQuestions/TestExceptionPropagation1.java
@@ -0,0 +1,27 @@
+//Program of Exception Propagation:
+//Rule: By default Unchecked Exceptions are forwarded in calling chain (propagated).
+package ExceptionAndStringInterviewQuestions;
+
+class TestExceptionPropagation1 {
+	void m() {
+		int data = 50 / 0;
+	}
+
+	void n() {
+		m();
+	}
+
+	void p() {
+		try {
+			n();
+		} catch (Exception e) {
+			System.out.println("exception handled");
+		}
+	}
+
+	public static void main(String args[]) {
+		TestExceptionPropagation1 obj = new TestExceptionPropagation1();
+		obj.p();
+		System.out.println("normal flow...");
+	}
+}
---
+++ src/ExceptionAndStringInterviewQuestions/TestExceptionPropagation2.java
@@ -0,0 +1,27 @@
+//Program which describes that checked exceptions are not propagated:
+//Rule: By default, Checked Exceptions are not forwarded in calling chain (propagated).
+package ExceptionAndStringInterviewQuestions;
+
+class TestExceptionPropagation2 {
+	void m() {
+		throw new java.io.IOException("device error");// checked exception
+	}
+
+	void n() {
+		m();
+	}
+
+	void p() {
+		try {
+			n();
+		} catch (Exception e) {
+			System.out.println("exception handeled");
+		}
+	}
+
+	public static void main(String args[]) {
+		TestExceptionPropagation2 obj = new TestExceptionPropagation2();
+		obj.p();
+		System.out.println("normal flow");
+	}
+}
